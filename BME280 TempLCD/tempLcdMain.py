b904b64f476948cfcc0f9f7ef8e25e979bac42e7
Vincenzo Pimpinella <vincenzo.pimpinella@gmail.com>
2017-03-24 23:31:13 +0100

    Update tempLcd

--- BME280 TempLCD/tempLcdMain.py
+++ BME280 TempLCD/tempLcdMain.py
@@ -1,8 +1,44 @@
 from Adafruit_BME280 import *
 import RPi_I2C_driver
+import signal
+import sys
+from time import *
+import time
+
+
+class BmeData:
+    def __init__(self, temperature, humidity, pressure):
+         self.temperature = temperature
+         self.humidity = humidity
+         self.pressure = pressure
+
+    def printData(self):
+        print 'Temp      = {0:0.3f} C'.format(self.temperature)
+        print 'Pressure  = {0:0.2f} hPa'.format(self.pressure)
+        print 'Humidity  = {0:0.2f} %'.format(self.humidity)
+
+    def printDataLcd(self, lcd):
+        lcd.lcd_display_string("Temp   = {0:0.2f} C".format(self.temperature),1)
+        lcd.lcd_display_string("Press  = {0:0.2f} hPa".format(self.pressure), 2)
+        lcd.lcd_display_string("Humi   = {0:0.2f} %".format(self.humidity), 3)    
+
+def readData(sensor):
+    return BmeData(sensor.read_temperature(), sensor.read_humidity(), sensor.read_pressure()/100)
 
 def main():
-    # my code here
+    sensor = BME280(mode=BME280_OSAMPLE_8)
+    mylcd = RPi_I2C_driver.lcd()
+    mylcd.lcd_clear()
+    while True:
+        try: 
+            readData(sensor).printDataLcd(mylcd)
+            time.sleep(2)
+        except KeyboardInterrupt:                    
+            mylcd.lcd_clear()
+            time.sleep(1)
+            mylcd.backlight(0)
+            sys.exit()
+    
 
 if __name__ == "__main__":
     main()
