5a36e79ec99b231ecdf5dabad94cc009b0db2bfa
Vincenzo Pimpinella <vincenzo.pimpinella@gmail.com>
2017-03-24 23:55:56 +0100

    Code style update

--- BME280 TempLCD/tempLcdMain.py
+++ BME280 TempLCD/tempLcdMain.py
@@ -1,39 +1,40 @@
 from Adafruit_BME280 import *
 import RPi_I2C_driver
-import signal
-import sys
-from time import *
-import time
 
+class BmeData(object):
+    """Data from BME280 sensor"""
 
-class BmeData:
     def __init__(self, temperature, humidity, pressure):
-         self.temperature = temperature
-         self.humidity = humidity
-         self.pressure = pressure
+        self.temperature = temperature
+        self.humidity = humidity
+        self.pressure = pressure
 
-    def printData(self):
+    def print_data(self):
+        """Write data to string"""
         print 'Temp      = {0:0.3f} C'.format(self.temperature)
         print 'Pressure  = {0:0.2f} hPa'.format(self.pressure)
         print 'Humidity  = {0:0.2f} %'.format(self.humidity)
 
-    def printDataLcd(self, lcd):
-        lcd.lcd_display_string("Temp   = {0:0.2f} C".format(self.temperature),1)
+    def print_data_lcd(self, lcd):
+        """Write data to lcd"""
+        lcd.lcd_display_string("Temp   = {0:0.2f} C".format(self.temperature), 1)
         lcd.lcd_display_string("Press  = {0:0.2f} hPa".format(self.pressure), 2)
-        lcd.lcd_display_string("Humi   = {0:0.2f} %".format(self.humidity), 3)    
+        lcd.lcd_display_string("Humi   = {0:0.2f} %".format(self.humidity), 3)
 
-def readData(sensor):
+def read_data(sensor):
+    """Read data from sensor"""
     return BmeData(sensor.read_temperature(), sensor.read_humidity(), sensor.read_pressure()/100)
 
 def main():
+    """Main"""
     sensor = BME280(mode=BME280_OSAMPLE_8)
     mylcd = RPi_I2C_driver.lcd()
     mylcd.lcd_clear()
     while True:
-        try: 
-            readData(sensor).printDataLcd(mylcd)
+        try:
+            read_data(sensor).print_data_lcd(mylcd)
             time.sleep(2)
-        except KeyboardInterrupt:                    
+        except KeyboardInterrupt:
             mylcd.lcd_clear()
             time.sleep(1)
             mylcd.backlight(0)
