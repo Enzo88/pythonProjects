730ab881d0b19fc734af9f352a979ab25e20a96f
Vincenzo Pimpinella <vincenzo.pimpinella@gmail.com>
2017-03-25 12:29:25 +0100

    feat: rename  + change port to 8080

--- BME280_TempLCD/rest_Server.py
+++ BME280_TempLCD/rest_Server.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
-from tempLcdMain import *
-import tempLcdMain
+from temp_Lcd_Main import *
+import temp_Lcd_Main
 import json
 app = Flask(__name__)
 
@@ -9,7 +9,8 @@ app = Flask(__name__)
 def get_sensor_data():
     """Get data from BME sensor"""
     sensor = BME280(mode=BME280_OSAMPLE_8)
-    return json.dumps(tempLcdMain.read_data(sensor).__dict__)
+    return json.dumps(temp_Lcd_Main.read_data(sensor).__dict__)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
